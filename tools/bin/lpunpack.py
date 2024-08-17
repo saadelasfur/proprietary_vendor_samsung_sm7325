288edfbf49f8c8847f9905d9e50b013580171ca5
Saad <saadelasfur@gmail.com>
2024-08-17 10:14:09 +0300

    tools/bin/lpunpack.py: update

--- tools/bin/lpunpack.py
+++ tools/bin/lpunpack.py
@@ -487,6 +487,11 @@ class Metadata:
     def _get_info(self) -> Dict:
         # TODO 25.01.2023: Liblp version 1.2 build_header_flag_string check header version 1.2
         result = {}
+        def get_size(index):
+            try:
+                return self.extents[index].num_sectors
+            except:
+                return 0
         try:
             result = {
                 "metadata_version": f"{self.header.major_version}.{self.header.minor_version}",
@@ -517,7 +522,7 @@ class Metadata:
                         "name": item.name,
                         "group_name": self.groups[item.group_index].name,
                         "is_dynamic": True,
-                        "size": self.extents[item.first_extent_index].num_sectors * LP_SECTOR_SIZE,
+                        "size": (get_size(item.first_extent_index)) * LP_SECTOR_SIZE,
                         "attributes": build_attribute_string(item.attributes),
                         "extents": self._get_extents_string(item)
                     } for item in self.partitions
@@ -643,19 +648,22 @@ class SparseImage:
                     if chunk_header.chunk_type == 0xCAC2:
                         data = self._read_data(chunk_data_size)
                         len_data = sector_size << 9
-                        out.write(struct.pack("B", 0) * len_data)
+                        out.truncate(out.tell() + len_data)
+                        out.seek(0, 2)
                         output_len += len(data)
                         sector_base += sector_size
                     else:
                         if chunk_header.chunk_type == 0xCAC3:
                             data = self._read_data(chunk_data_size)
                             len_data = sector_size << 9
-                            out.write(struct.pack("B", 0) * len_data)
+                            out.truncate(out.tell() + len_data)
+                            out.seek(0, 2)
                             output_len += len(data)
                             sector_base += sector_size
                         else:
                             len_data = sector_size << 9
-                            out.write(struct.pack("B", 0) * len_data)
+                            out.truncate(out.tell() + len_data)
+                            out.seek(0, 2)
                             sector_base += sector_size
                 chunks -= 1
         return unsparse_file
@@ -867,7 +875,7 @@ def create_parser():
         '-p',
         '--partition',
         dest='NAME',
-        type=lambda x: re.split("\W+", x),
+        type=lambda x: re.split("\\W+", x),
         help='Extract the named partition. This can be specified multiple times or through the delimiter [","  ":"]'
     )
     _parser.add_argument(
